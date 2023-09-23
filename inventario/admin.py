491edd5bafcf36ca84fe3b5b5eb158ffa5cabf05
marcelo.arteaga <marcelo.arteaga@elevatesecurity.com>
2023-09-22 21:43:13 -0400

    clase 3

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria
+from .models import Producto
 
-# Register your models here.
+admin.site.register(Categoria)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'precio', 'unidades', 'disponible')
+    ordering = ['precio']
+    search_fields = ['nombre']
+    list_filter = ['disponible']
+
+admin.site.register(Producto, ProductoAdmin)
--- inventario/views.py
+++ inventario/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Categoria
 
 
 def index(request):
     return HttpResponse("Hola Mundo")
+
+def contact(request, name):
+    return HttpResponse(f"Bienvenido {name} a la clase de Django")
+
+def categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, "categorias.html",{
+        "titulo": "Listado de Categorias",
+        "categorias": categorias
+    })
